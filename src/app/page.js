2d299bf261fcdb0237f4aceeb146d730a0a22563
safi-sadi <sadisafi480@gmail.com>
2026-08-07 22:30:51 +0600

    Fix TeamSection import path

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import About from "@/components/sections/home/About/About";
 import Services from "@/components/sections/home/Services/Services";
 import WhyChooseUs from "@/components/sections/home/whychoose/WhyChooseUs";
 import TestimonialsSection from "@/components/sections/home/Testimonials/TestimonialsSection";
-import TeamSection from "@/components/sections/home/team/TeamSection";
+import TeamSection from "@/components/sections/home/Team/TeamSection";
 import CoursesSection from "@/components/sections/home/Courses/CoursesSection";
 import CEOSection from "@/components/sections/home/CEOMessage/CEOSection";
 
