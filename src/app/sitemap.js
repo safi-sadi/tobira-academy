ed092f952d7c2e12f3e40aba73c3683fbd5414d8
safi-sadi <sadisafi480@gmail.com>
2026-09-02 20:50:22 +0600

    updated sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,5 +1,5 @@
 export default function sitemap() {
-  const baseUrl = "https://www.tobirajla.com/";
+  const baseUrl = "https://www.tobirajla.com";
 
   return [
     {
@@ -8,64 +8,88 @@ export default function sitemap() {
       changeFrequency: "weekly",
       priority: 1,
     },
+
     {
       url: `${baseUrl}/about`,
       lastModified: new Date(),
+      changeFrequency: "weekly",
       priority: 0.9,
     },
+
     {
       url: `${baseUrl}/services`,
       lastModified: new Date(),
+      changeFrequency: "weekly",
       priority: 0.9,
     },
+
     {
       url: `${baseUrl}/courses`,
       lastModified: new Date(),
+      changeFrequency: "weekly",
       priority: 0.9,
     },
+
     {
       url: `${baseUrl}/gallery`,
       lastModified: new Date(),
+      changeFrequency: "weekly",
       priority: 0.9,
     },
+
     {
       url: `${baseUrl}/services/student-visa-processing`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
       priority: 0.8,
     },
+
     {
       url: `${baseUrl}/services/university-admission-support`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
       priority: 0.8,
     },
+
     {
       url: `${baseUrl}/services/language-preparation`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
       priority: 0.8,
     },
+
     {
       url: `${baseUrl}/services/scholarship-assistance`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
       priority: 0.8,
     },
+
     {
       url: `${baseUrl}/services/accommodation-support`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
       priority: 0.8,
     },
+
     {
       url: `${baseUrl}/services/pre-departure-support`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
       priority: 0.8,
     },
+
     {
       url: `${baseUrl}/courses/japanese-language-n5`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
       priority: 0.8,
     },
+
     {
       url: `${baseUrl}/courses/japanese-language-n4`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
       priority: 0.8,
     },
   ];
